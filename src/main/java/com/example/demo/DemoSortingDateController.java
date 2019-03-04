3e69331c913ca15f2db612647332551fe09aff84
Francesco Losciale <francesco.losciale@gmail.com>
2019-03-04 21:16:23 +0000

    Initial commit

--- src/main/java/com/example/demo/DemoSortingDateController.java
+++ src/main/java/com/example/demo/DemoSortingDateController.java
@@ -1,21 +1,37 @@
 package com.example.demo;
 
-import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collections;
+import java.util.List;
+
 @Controller
 public class DemoSortingDateController {
 
-    @RequestMapping(value = "date", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    @RequestMapping(value = "/date/save", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     @ResponseBody
-    public List<LocalDate> save(@RequestBody DemoDateSet demoDateSet) {
+    public String save(@RequestBody DemoDateSet demoDateSet) {
+        return "Done";
+    }
+
+    @RequestMapping("/greeting")
+    public @ResponseBody String greeting() {
+        return "Thanks for calling";
+    }
+
+//    @RequestMapping(value="sorted", method = RequestMethod.GET)
+//    public List<DemoDate> sorted() {
+//        return Collections.emptyList();
+//    }
+
+//    @RequestMapping(value="/Object/getList/", method=RequestMethod.GET)
+    @RequestMapping(value="/date/sorted", method=RequestMethod.GET)
+    public @ResponseBody List<Object> findAllObjects() {
         return Collections.emptyList();
     }
+
 }
--- src/test/java/com/example/demo/JavaSpringMvcApplicationTests.java
+++ src/test/java/com/example/demo/JavaSpringMvcApplicationTests.java
@@ -2,15 +2,64 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JavaSpringMvcApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private DemoSortingDateController demoSortingDateController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    // TODO https://spring.io/guides/gs/testing-web/
 
     @Test
     public void contextLoads() {
+        assertThat(demoSortingDateController).isNotNull();
+    }
+    
+    @Test
+    public void testCallIsWorking() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/greeting",
+                String.class)).contains("Thanks for calling");
+    }
+
+    @Test
+    public void testSubmit() {
+        DemoDate demoDate = new DemoDate();
+        demoDate.setId(1L);
+        DemoDateSet demoDateSet = new DemoDateSet();
+        demoDateSet.setDates(Arrays.asList(demoDate));
+        this.restTemplate.postForEntity("http://localhost:" + port + "/date/save", demoDateSet, String.class);
+    }
+
+    @Test
+    public void testGetSortedList() {
+        // https://stackoverflow.com/questions/23674046/get-list-of-json-objects-with-spring-resttemplate
+        ResponseEntity<DemoDate[]> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/date/sorted", DemoDate[].class);
+        DemoDate[] objects = responseEntity.getBody();
+        MediaType contentType = responseEntity.getHeaders().getContentType();
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        assertEquals(HttpStatus.OK, statusCode);
     }
 
 }
