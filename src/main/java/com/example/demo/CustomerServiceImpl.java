3568cedade738d4d0e1d9fc6aa38cee4e8a5eca8
ealfors <francesco.losciale@ericsson.com>
2019-03-07 18:49:18 +0000

    removed unused import

--- src/main/java/com/example/demo/CustomerServiceImpl.java
+++ src/main/java/com/example/demo/CustomerServiceImpl.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
